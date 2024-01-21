9348e540c30c054b040a0097673aadeb62ddb1bd
yunAyse <aysepc0@gmail.com>
2024-01-21 19:37:27 +0100

    next and prev button working.

---
+++ main.js
@@ -0,0 +1,36 @@
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+
+// config param 
+let countItem = items.length;
+let itemActive = 0;
+
+// event next click 
+next.onclick = function() {
+  itemActive = itemActive + 1;
+  if (itemActive >= countItem) {
+    itemActive = 0;
+  }
+  showSlider();
+}
+
+//event prev click
+prev.onclick = function() {
+  itemActive = itemActive - 1;
+  if (itemActive < 0) {
+    itemActive = countItem - 1;
+  }
+  showSlider();
+}
+
+function showSlider(){
+  // remove item active old
+  let itemActiveOld = document.querySelector('.slider .list .item.active');
+  itemActiveOld.classList.remove('active');
+
+  // active new item
+  items[itemActive].classList.add('active');
+}
+
+//
